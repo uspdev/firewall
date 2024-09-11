8f840ae3ba4fcda0ba51ef65876d34810fd8053e
Masaki Kawabata Neto <kawabata@usp.br>
2024-09-11 17:31:44 -0300

    formatando arquivo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,12 @@
 @extends('layouts.app')
 
-@section('title') Firewall @endsection
-
 @section('content')
-
-
-<div class="h3">
+  <div class="h3">
     Endereço IP atual: {{ $user->ip }}<br>
-</div>
-<br>
-@include('partials.rules')
-<br>
-@include('partials.activity')
+  </div>
+  <br>
+  @include('partials.rules')
+  <br>
+  @include('partials.activity')
+  
 @endsection
-
