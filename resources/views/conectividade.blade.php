0c044fed836faccee35181858bceea6c34356cee
Masaki Kawabata Neto <kawabata@usp.br>
2024-11-25 08:14:32 -0300

    mostrando ip para não logado

--- resources/views/conectividade.blade.php
+++ resources/views/conectividade.blade.php
@@ -2,34 +2,37 @@
 
 @section('content')
   @can('admin')
-    <h1>Instruções para configurar o servidor pfsense</h1>
-    <h6>{{ $msg }}</h6>
-    <h5>O servidor não está configurado corretamente.</h5>
-    <h5>Abaixo estão instruções possíveis de como solucionar o devido erro:</h5>
+    <div class="alert alert-danger">{{ $msg }}</div>
+
+    <div>O servidor não está acessível ou não está configurado corretamente!</div>
+    <div>
+      FIREWALL_SSH: {{ config('firewall.ssh') }}<br>
+      PRIVATE_KEY: {{ config('firewall.private_key') }}
+    </div>
     <hr>
     <h6>(Lembrete: a comunicação ssh deve sempre ser feita utilizando uma chave)</h6>
     <div class="card">
-      <div class="card-body">
-        <h3>Erro no arquivo de ambiente (.env)</h3>
-        <p>Se o erro está no arquivo de ambiente .env, o endereço do servidor fornecido nesse
-          arquivo provavelmente tem algo de errado.</p>
-        <p>Verifique o endereço novamente e garanta que esteja no formato user@ip-do-pfsense.</p>
+      <div class="card-body py-2">
+        <div class="h5">Erro no arquivo de ambiente (.env)</div>
+        Se o erro está no arquivo de ambiente .env, o endereço do servidor fornecido nesse
+        arquivo provavelmente tem algo de errado.<br>
+        Verifique o endereço novamente e garanta que esteja no formato user@ip-do-pfsense.
       </div>
     </div>
     <br>
     <div class="card">
-      <div class="card-body">
-        <h3>Erro de Host Down</h3>
-        <p>Se o endereço está correto, o ip que o firewall está tentando não está funcionando.</p>
-        <p>Verifique o status e a estabilidade do host indicado no .env</p>
+      <div class="card-body py-2">
+        <div class="h5">Erro de Host Down</div>
+        Se o endereço está correto, o ip que o firewall está tentando não está funcionando.<br>
+        Verifique o status e a estabilidade do host indicado no .env
       </div>
     </div>
     <br>
     <div class="card">
-      <div class="card-body">
-        <h3>Erro de SSH</h3>
-      <p>O endereço que o firewall está tentando acessar funciona, mas não está sendo possível estabelecer uma conexão.</p>
-        <p>Verifique o sistema do host e o usuário, indicado no .env</p>
+      <div class="card-body py-2">
+        <div class="h5">Erro de SSH</div>
+        O endereço que o firewall está tentando acessar funciona, mas não está sendo possível estabelecer uma conexão.<br>
+        Verifique o sistema do host e o usuário, indicado no .env
       </div>
     </div>
   @endcan
--- resources/views/nologin.blade.php
+++ resources/views/nologin.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
 @section('content')
-Autentique-se para acessar o sistema!
+Autentique-se para acessar o sistema!<br>
+<br>
+Seu IP atual é {{ $ip }}
 @endsection
